0bcd0541137b2561187fe10c4723ea3ffae7e4ac
David Howard <dhowardjava@gmail.com>
2020-06-25 14:16:12 -0400

    Created a class for storing logged in user information to be used throughout the application. These will be used for making API calls and UI. Also added MVVM Light for using messenger services.

---
+++ CreedCollector.ViewModels/LoggedInProperties/UserInformation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreedCollector.ViewModels.LoggedInProperties
+{
+    // Properties to be used by class after user is logged in.
+    public class UserInformation
+    {
+        public static string FirstName { get; set; }
+        public static string XboxLiveGamertag { get; set; }
+        public static string SteamUsername { get; set; }
+        public static string PsnName { get; set; }
+    }
+}
--- CreedCollector.ViewModels/LoginWindowViewModel.cs
+++ CreedCollector.ViewModels/LoginWindowViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using CreedCollector.Models;
 using CreedCollector.ViewModels.Commands;
 using CreedCollector.ViewModels.Hashing;
+using CreedCollector.ViewModels.LoggedInProperties;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Security;
 using System.Text;
@@ -60,6 +62,9 @@ namespace CreedCollector.ViewModels
             LoginCommand = new LoginCommand(this);
         }
 
+        /* This method will check if the user exists in the DB and also check the password. Password is salted and hashed.
+         * After user has been authenticated with the DB that user's information which is need for the application is stored
+         * in static properties in a seperate class, the method which stores the properties is SetUserInformation. */
         public void Login()
         {
             using (CreedCollectorEntities ctx = new CreedCollectorEntities())
@@ -81,6 +86,22 @@ namespace CreedCollector.ViewModels
                     MessageBox.Show("Incorrect Password Entered.");
                     return;
                 }
+                SetUserInformation();
+            }
+        }
+
+        // This method will set the User information in static methods which will be used throughout the application.
+        private void SetUserInformation()
+        {
+            using (CreedCollectorEntities ctx = new CreedCollectorEntities())
+            {
+                User user = ctx.Users
+                               .Where(User => User.UserName == this.UserName)
+                               .FirstOrDefault();
+                UserInformation.FirstName = user.FirstName;
+                UserInformation.XboxLiveGamertag = user.XboxLiveGamertag;
+                UserInformation.SteamUsername = user.SteamId;
+                UserInformation.PsnName = user.PlaystationNetworkId;
             }
         }
 
