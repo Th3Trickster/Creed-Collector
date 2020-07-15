f54c2b9e78b7f89aaf9477e20cc121bcf6f70965
David Howard <dhowardjava@gmail.com>
2020-07-15 15:17:29 -0400

    Finished GameCover user control for adding to the GamesView. Also added a test GameCover.

--- CreedCollector/UserControls/GameCover.xaml.cs
+++ CreedCollector/UserControls/GameCover.xaml.cs
@@ -26,6 +26,56 @@ namespace CreedCollector.UserControls
             set { SetValue(CoverImageProperty, value); }
         }
 
+        public string PlatformLogoOne
+        {
+            get { return (string)GetValue(PlatformLogoOneProperty); }
+            set { SetValue(PlatformLogoOneProperty, value); }
+        }
+
+        public string PlatformLogoTwo
+        {
+            get { return (string)GetValue(PlatformLogoTwoProperty); }
+            set { SetValue(PlatformLogoTwoProperty, value); }
+        }
+
+        public string PlatformLogoThree
+        {
+            get { return (string)GetValue(PlatformLogoThreeProperty); }
+            set { SetValue(PlatformLogoThreeProperty, value); }
+        }
+
+        public string CardButtonIcon
+        {
+            get { return (string)GetValue(CardButtonIconProperty); }
+            set { SetValue(CardButtonIconProperty, value); }
+        }
+
+        public string GameTitle
+        {
+            get { return (string)GetValue(GameTitleProperty); }
+            set { SetValue(GameTitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for GameTitle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty GameTitleProperty =
+            DependencyProperty.Register("GameTitle", typeof(string), typeof(GameCover));
+
+        // Using a DependencyProperty as the backing store for CardButtonIcon.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CardButtonIconProperty =
+            DependencyProperty.Register("CardButtonIcon", typeof(string), typeof(GameCover));
+
+        // Using a DependencyProperty as the backing store for PlatformLogoThree.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PlatformLogoThreeProperty =
+            DependencyProperty.Register("PlatformLogoThree", typeof(string), typeof(GameCover));
+
+        // Using a DependencyProperty as the backing store for PlatformLogoTwo.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PlatformLogoTwoProperty =
+            DependencyProperty.Register("PlatformLogoTwo", typeof(string), typeof(GameCover));
+
+        // Using a DependencyProperty as the backing store for PlatformLogoOne.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PlatformLogoOneProperty =
+            DependencyProperty.Register("PlatformLogoOne", typeof(string), typeof(GameCover));
+
         // Using a DependencyProperty as the backing store for CoverImage.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty CoverImageProperty =
             DependencyProperty.Register("CoverImage", typeof(string), typeof(GameCover));
